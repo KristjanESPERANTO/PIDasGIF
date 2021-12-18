8417c4fbe1233d04bb183ca44a529a010e0b649a
Kristjan SCHMIDT <35647502+KristjanESPERANTO@users.noreply.github.com>
2021-12-18 14:28:35 +0100

    First halfway presentable

---
+++ app.js
@@ -0,0 +1,52 @@
+
+    let canvasHeight;
+    let canvasWidth;
+
+    let canvasButton = document.getElementById('canvasButton');
+    canvasButton.addEventListener('click', addCanvas);
+
+    let createAnimationButton = document.getElementById('createAnimationButton');
+    createAnimationButton.addEventListener('click', mergeCanvases);
+    createAnimationButton.hidden = true;
+
+    let gifAnimationContainer = document.getElementById('gifAnimationContainer');
+    gifAnimationContainer.hidden = true;
+
+
+    function addCanvas() {
+
+      element = document.getElementById("wrapper");
+      const lineBreak = document.createElement('br');
+
+      html2canvas(element).then(function (canvas) {
+        canvasHeight = canvas.height;
+        canvasWidth = canvas.width;
+        createAnimationButton.hidden = false;
+        document.body.appendChild(lineBreak);
+        document.body.appendChild(canvas);
+      });
+    }
+
+    function mergeCanvases() {
+      var encoder = new GIFEncoder();
+      encoder.setRepeat(0);
+      encoder.setDelay(1500);
+      canvases = document.getElementsByTagName("canvas");
+
+      encoder.setSize(canvasWidth, canvasHeight);
+      console.log(encoder.start());
+
+      for (let canvas of canvases) {
+        if (canvas.id != "bitmap") {
+          console.log(canvas.height + " " + canvas.width);
+          let context = canvas.getContext('2d');
+          encoder.addFrame(context);
+        }
+      }
+
+      encoder.finish();
+      document.getElementById('gifAnimation').src = 'data:image/gif;base64,' + encode64(encoder.stream().getData());
+      document.getElementById('gifAnimation').width = canvasWidth;
+      document.getElementById('gifAnimation').height = canvasHeight;
+      gifAnimationContainer.hidden = false;
+    }
---
+++ readme.md
@@ -0,0 +1,29 @@
+# PIDasGIF
+
+Create an animated GIF file to simulate a passenger information display.
+
+## Exemplary result
+![Exemplary result](images/example01.gif "Exemplary result")
+
+## Usage
+
+1. Adapt the content of the table
+2. Press button to add this as slide
+3. Repeat steps 1 and 2 until you have all slides you need
+4. Use button `Create Animation` to create the GIF file
+5. Use right click to download the GIF file
+
+## Todo
+
+- Working Radio Buttons
+- Details for Third-Party
+- GitHub website
+- GitHub button in index.html
+
+## Third-Party
+
+- 24 LED
+- html2canvas
+- jsgif
+
+## Third-Party
